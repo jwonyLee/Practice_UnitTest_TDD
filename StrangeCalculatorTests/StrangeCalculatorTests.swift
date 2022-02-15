bb21abe632cca13c9529f10ace92792b13a1f44f
JiWon Lee <jiwon.swdev@gmail.com>
2022-02-15 10:52:28 +0900

    test: 0을 넣으면 0을 반환한다

--- StrangeCalculatorTests/StrangeCalculatorTests.swift
+++ StrangeCalculatorTests/StrangeCalculatorTests.swift
@@ -20,4 +20,20 @@ class StrangeCalculatorTests: XCTestCase {
     }
 
     // TODO: TDD로 정수 타입을 파라미터로 받아, 그 절대값이 홀수이면 부호를 바꾸어서 반환하는 메서드 구현하기
+    func test_0을_넣으면_0을_반환한다() {
+        // given
+        let input: Int = 0
+        let expectation: Int = 0
+        
+        // when
+        let result: Int = strangeCalculator?.changeSignIfAbsoluteNumberIsOdd(to: input) ?? 0
+        
+        // result
+        XCTAssertEqual(result, expectation)
+    }
+    
+    // TODO: 0을 넣으면 0을 반환한다
+    // TODO: 10을 넣으면 10을 반환한다
+    // TODO: 3을 넣으면 -3을 반환한다
+    // TODO: -3을 넣으면 3을 반환한다
 }
--- UnitTestSample/Practice/StrangeCalculator.swift
+++ UnitTestSample/Practice/StrangeCalculator.swift
@@ -7,4 +7,7 @@ import Foundation
 
 struct StrangeCalculator {
     // TODO: TDD로 정수 타입을 파라미터로 받아, 그 절대값이 홀수이면 부호를 바꾸어서 반환하는 메서드 구현하기
+    func changeSignIfAbsoluteNumberIsOdd(to number: Int) -> Int {
+        return 0
+    }
 }

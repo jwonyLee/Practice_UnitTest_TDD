34963b721ca424be96cb332ca416c709f6be7784
JiWon Lee <jiwon.swdev@gmail.com>
2022-02-15 10:19:21 +0900

    test: 테스트를 위한 구성

--- LottoMachineTests/LottoMachineTests.swift
+++ LottoMachineTests/LottoMachineTests.swift
@@ -6,25 +6,29 @@
 import XCTest
 
 class LottoMachineTests: XCTestCase {
-        
+    var lottoMachine: LottoMachine?
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try setUpWithError()
+        lottoMachine = LottoMachine()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try tearDownWithError()
+        lottoMachine = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    // MARK: - isValidLottoNumbers(of:)
+    // TODO: 로또 숫자의 개수가 6개 이하인 경우 false를 반환한다
+    // TODO: 로또 숫자의 개수가 6개 이상인 경우 false를 반환한다
+    // TODO: 로또 숫자의 개수가 6개가 넘으면서 중복이 있는 경우 false를 반환한다
+    // TODO: 로또 숫자의 개수가 6개 이면서 중복이 있는 경우 false를 반환한다
+    // TODO: 로또 숫자의 범위가 1 ~ 45 내에 없는 경우 false를 반환한다
+    
+    // MARK: - makeRandomLottoNumbersArray()
+    // TODO: 여섯 개의 숫자를 반환한다
+    
+    // MARK: - countMatchingNumber(user:winner:)
+    // TODO: user와 winner의 숫자가 1개만 일치한 경우 1을 반환한다
+    // TODO: user와 winner의 숫자가 로또 숫자가 아닌 경우 LottoMachineError.invalidNumbers를 던진다.
 }
